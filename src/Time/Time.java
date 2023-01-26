08f75ccd2336fb68abcb78a12af3b0f98de850c2
firetyler <oliver-z-jonsson@hotmail.com>
2023-01-26 14:35:09 +0100

    interface added and stuff

--- src/Time/Time.java
+++ src/Time/Time.java
@@ -1,4 +1,46 @@
 package Time;
 
-public class Time {
+public class Time implements TimeInterFace{
+    public Time(){
+
+    }
+    @Override
+    public String setTime() {
+        return null;
+    }
+
+    @Override
+    public String getTime() {
+        return null;
+    }
+
+    @Override
+    public int getHour() {
+        return 0;
+    }
+
+    @Override
+    public int setHour() {
+        return 0;
+    }
+
+    @Override
+    public int setMinuts() {
+        return 0;
+    }
+
+    @Override
+    public int getMinuts() {
+        return 0;
+    }
+
+    @Override
+    public int setSecunds() {
+        return 0;
+    }
+
+    @Override
+    public int getSecunds() {
+        return 0;
+    }
 }
--- src/Time/TimeInterFace.java
+++ src/Time/TimeInterFace.java
@@ -1,4 +1,13 @@
 package Time;
 
 public interface TimeInterFace {
+    public String setTime();
+    public String getTime();
+    public int getHour();
+    public int setHour();
+
+    public int setMinuts ();
+    public int getMinuts ();
+    public int setSecunds();
+    public int getSecunds();
 }
