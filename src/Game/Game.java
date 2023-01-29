ecbc2fe9775f0b0f755696ecea0ada99a4deece2
David <davidsjoblom@hotmail.se>
2023-01-29 13:41:09 +0100

    v1

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -5,26 +5,46 @@ import Player.PlayerInterFace;
 import Player.Player;
 import Time.Time;
 
-public class Game {
+import java.util.Scanner;
 
+public class Game {
+    private static boolean running = true;
     public static void main(String[] args) {
+
         PlayerInterFace player1 = new Player();
         PlayerInterFace player2 = new Player();
 
         Clock player1Clock = player1.getClock();
         Clock player2Clock = player2.getClock();
-        Time time = new Time(0,0,5);
+        Time time = new Time(0, 0, 5);
         player1Clock.setTime(time);
         player2Clock.setTime(time);
 
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Player 1's clock is running");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter 'stop' to stop the thread: ");
+            String input = scanner.nextLine();
+            if (input.equals("s")) {
+                running = false;
             }
-        });
-
-
+            else if (input.equals("r")) {
+                running = true;
+                runner();
+            }
+        }
     }
 
+    private static void runner(){
+        Thread t1 = new Thread(() -> {
+            while (running) {
+                System.out.println("Running");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        t1.start();
+    }
 }
